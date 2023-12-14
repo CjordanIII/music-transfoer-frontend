bcc1741e002b4d3c6cccc98c10b89a42a6db214d
Clarence Jordan <clarencejordan087@gmail.com>
2023-12-14 02:27:16 -0600

    the read,delete and and some what create is done

--- src/components/EditMusicComponet.js
+++ src/components/EditMusicComponet.js
@@ -1,60 +1,33 @@
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 const EditMusicComponent = ({ props }) => {
   const { playlistName, songsOnPlaylist } = props.playlist;
-  const form = useForm();
-  // register captures data takes in dynamicly"name[{name}]" staticly "names"
-  const { register, handleSubmit } = form;
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {
+      playlistName,
+      songs: songsOnPlaylist.songs.map((song, index) => ({
+        name: song,
+        artists: songsOnPlaylist.artist[index].map((data) => ({
+          name: data.name,
+        })),
+      })),
+    },
+  });
 
-  const combinedData = songsOnPlaylist.songs.map((song, index) => ({
-    song,
-    artists: songsOnPlaylist.artist[index].map((data) => data.name),
-  }));
-
-  function getItSorted(data) {
-    
-    const songsEntries = Object.entries(data);
-try{
-    const filted = songsEntries.filter(([key, value]) => {
-      if (value !== "") {
-        return [key, value];
-      }
-    });
-    const filtedMore = filted.filter(([key,value])=>{
-      console.log(value.length);
-      if(value.length>0){
-        return [key,value]
-      }
-    })
-    const objectAgain = Object.fromEntries(filtedMore)
-    return objectAgain
-}catch(e){
-  console.log(e)
-}
-
-    
-  }
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "songs",
+  });
 
   const onSubmit = (data) => {
     // Handle form submission logic here
-    
-    try{
-      const songData = getItSorted(data.songs);
-       console.log(songData)
-    }catch(e){
-      console.log(e)
-    }
-   
-
-
+    console.log(data);
   };
 
   return (
-    // handle submit built in function (method ig) takes my funtion for logic
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* Edit Playlist Name */}
       <label
-        htmlFor="Playlist-name"
+        htmlFor="playlistName"
         className="block text-lg font-medium text-gray-700"
       >
         Edit playlist name
@@ -66,48 +39,60 @@ try{
         className="mt-2 w-full rounded-md py-2 px-2 border-gray-200 shadow-sm sm:text-sm"
       />
 
-      {/* Edit Songs and Artists */}
-      {combinedData.map(({ song, artists }) => (
-        <div className="flex" key={song}>
-          {/* Edit Song */}
+      {fields.map((song, songIndex) => (
+        <div className="flex" key={songIndex}>
           <label
-            htmlFor={song}
+            htmlFor={`songs[${songIndex}].name`}
             className="block text-lg font-medium text-gray-700"
           >
             Edit song
           </label>
           <input
             type="text"
-            name={`songs[${song}]`}
-            {...register(`songs[${song}]`)}
-            placeholder={song}
+            {...register(`songs[${songIndex}].name`)}
+            placeholder={song.name}
             className="w-full rounded-md py-1 h-10 px-1 border-gray-200 shadow-sm sm:text-sm"
           />
 
-          {/* Edit Artists */}
-          {artists.map((artist, artistIndex) => (
-            <div key={artist}>
+          {song.artists.map((artist, artistIndex) => (
+            <div key={artistIndex}>
               <label
-                htmlFor={artist}
+                htmlFor={`songs[${songIndex}].artists[${artistIndex}].name`}
                 className="block text-lg font-medium text-gray-700"
               >
                 Edit Artist
               </label>
               <input
                 type="text"
-                name={`artists[${song}][${artistIndex}]`}
-                {...register(`artists[${song}][${artistIndex}]`)}
-                placeholder={artist}
+                {...register(
+                  `songs[${songIndex}].artists[${artistIndex}].name`
+                )}
+                placeholder={artist.name}
                 className="mt-2 w-full rounded-md h-10 py-2 px-2 border-gray-200 shadow-sm sm:text-sm"
               />
             </div>
           ))}
+
+          <button
+            type="button"
+            onClick={() => remove(songIndex)}
+            className="ml-2 bg-red-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Remove
+          </button>
         </div>
       ))}
 
-      {/* Submit Button */}
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="button"
+        onClick={() => append({ name: "", artists: [] })}
+        className="mt-2 bg-green-500 text-white font-bold py-2 px-4 rounded"
+      >
+        Add Song
+      </button>
+
+      <button
+        className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         type="submit"
       >
         Submit Changes
